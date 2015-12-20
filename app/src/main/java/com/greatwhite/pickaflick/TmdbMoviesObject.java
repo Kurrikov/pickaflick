5253b0d040526c44904df3eeddda3770b630c40d
Jason Curry <j971c055@ku.edu>
2015-12-20 14:54:25 -0600

    Removed Keon's implentation of Math.min() and improved comments

--- app/src/main/java/com/greatwhite/pickaflick/TmdbMoviesObject.java
+++ app/src/main/java/com/greatwhite/pickaflick/TmdbMoviesObject.java
@@ -18,27 +18,31 @@ public class TmdbMoviesObject extends TmdbObject {
         super(MPAA_Rating, genre, era_low, era_high, minRating);
     }
 
+    /**
+     * Set moviesList to the specified list of movies.
+     * @param movieDbs A list of movies
+     */
     public void setMoviesList(List<MovieDb> movieDbs){
         if(movieDbs != null){
             moviesList = new ArrayList<MovieAttributes>();
-            int minSize = minimum(movieDbs.size(),20);
+            int minSize = Math.min(movieDbs.size(), 20);
             for (int i = 0; i < minSize; i++) {
                 this.moviesList.add(new MovieAttributes(movieDbs.get(i)));
             }
         }
     }
 
-    private int minimum(int x, int y){
-        return x > y ? y : x;
-    }
-
     public List<MovieAttributes> getMoviesList() {
         return this.moviesList;
     }
 
-    // The default TMDb method does a poor job in filtering movies by years of release, so this method was added to rectify that issue
-    // This will also prioritize the order based on voteCounts and vote Averages (the Ratings) combined. This will hopefully manage
-    // to filter out lower quality movies that made their way into the list, such as those with too few votes to be considered legitimate
+    /**
+     * The default TMDb method does a poor job in filtering movies by years of release, so this method was added to rectify that issue.
+     * This will also prioritize the order based on voteCounts and vote Averages (the Ratings) combined. This will hopefully manage
+     * to filter out lower quality movies that made their way into the list, such as those with too few votes to be considered legitimate
+     * @param movieDbs A list of movies
+     * @return A corrected list of movies
+     */
     public List<MovieDb> listCorrector(List<MovieDb> movieDbs) {
 
         //remove "out of date" movies
@@ -49,8 +53,9 @@ public class TmdbMoviesObject extends TmdbObject {
         }
 
         //Sort by vote count and rating (descending order)
-        Collections.sort(movieDbs, new Comparator<MovieDb>() {      //in order to sort this list, we need a comparator that can compare the ratings and vote counts to each other.
-                                                                    // (There is way more than one way to do this.)
+        Collections.sort(movieDbs, new Comparator<MovieDb>() {
+        // In order to sort this list, we need a comparator that can compare the ratings and vote counts to each other.
+        // (There is way more than one way to do this.)
             @Override
             public int compare(MovieDb movieDb1, MovieDb movieDb2) {
                 return (int)(movieDb2.getVoteAverage()*Math.log10(movieDb2.getVoteCount()) - (movieDb1.getVoteAverage()*Math.log10(movieDb1.getVoteCount())));
